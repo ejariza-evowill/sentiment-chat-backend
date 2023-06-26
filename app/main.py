df7ce00a009aa9219d0795e7cda36c9c077be86e
Magnico <ejmrebolledo@gmail.com>
2023-06-26 12:15:22 -0500

    Created User Schema and Tests

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
+from app.routes import user, chat
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.models import Base
 
 app = FastAPI()
 
-# Import the routers
-from app.routes import user, chat
+# Configure the database connection
+DATABASE_URL = "sqlite:///./app.db"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
 
 # Include the routers
 app.include_router(user.router)
