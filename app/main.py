3d9865b61f6c228a69fc7e5a08a9b6e1cceffa1b
Magnico <ejmrebolledo@gmail.com>
2023-06-27 10:27:14 -0500

    Created config file

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import Base
+from app.config import DATABASE_URL
 
 app = FastAPI()
 
 # Configure the database connection
-DATABASE_URL = "sqlite:///./app.db"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
