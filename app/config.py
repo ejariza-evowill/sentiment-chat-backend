ee66d97ec31e3b9b4e653640f37cffc9566c1ef6
Javier Alejandro Gómez Muñoz <jaagomezmu@unal.edu.co>
2023-06-28 18:09:09 -0500

    "feat: add email service

--- app/config.py
+++ app/config.py
@@ -1,1 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '..', '.env')
+load_dotenv(dotenv_path)
+
 DATABASE_URL = "sqlite:///./test.db"
+SMTP_HOST = os.getenv('SMTP_HOST')
+SMTP_PORT = os.getenv('SMTP_PORT')
+SMTP_USERNAME = os.getenv('SMTP_USERNAME')
+SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from app.routes import user, chat
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from app.models import Base
+
 from app.config import DATABASE_URL
+from app.models import Base
+from app.routes import chat, email, user
 
 app = FastAPI()
 
@@ -17,3 +18,4 @@ Base.metadata.create_all(bind=engine)
 # Include the routers
 app.include_router(user.router)
 app.include_router(chat.router)
+app.include_router(email.router)
